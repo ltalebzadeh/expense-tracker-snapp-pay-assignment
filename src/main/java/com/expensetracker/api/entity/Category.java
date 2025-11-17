7ba6725cdbde2281e309a55d401943ee5eb3eb87
leila <l.talebzadeh93@gmail.com>
2025-11-17 23:14:13 +0330

    feat: Add Category entity

--- src/main/java/com/expensetracker/api/entity/Category.java
+++ src/main/java/com/expensetracker/api/entity/Category.java
@@ -1,4 +1,16 @@
 package com.expensetracker.api.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "categories")
+@Data
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
 }
