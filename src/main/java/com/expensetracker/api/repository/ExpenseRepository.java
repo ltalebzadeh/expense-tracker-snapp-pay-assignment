b070aec8ebf362d4129f3468f34909b05f911b9f
leila <l.talebzadeh93@gmail.com>
2025-11-18 00:09:10 +0330

    feat: Add repositories for the entities

---
+++ src/main/java/com/expensetracker/api/repository/ExpenseRepository.java
@@ -0,0 +1,9 @@
+package com.expensetracker.api.repository;
+
+import com.expensetracker.api.entity.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+}
