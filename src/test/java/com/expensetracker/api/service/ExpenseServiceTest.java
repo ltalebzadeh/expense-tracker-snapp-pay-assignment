ba12cab86e2a34592dba634998ae682e67536ae8
leila <l.talebzadeh93@gmail.com>
2025-11-18 00:26:45 +0330

    feat: Add exception handler

---
+++ src/test/java/com/expensetracker/api/service/ExpenseServiceTest.java
@@ -0,0 +1,12 @@
+package com.expensetracker.api.service;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpenseServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
