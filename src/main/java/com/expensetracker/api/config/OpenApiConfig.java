76c0b30a28551e97e78112fbdc0492baf1a2b25e
leila <l.talebzadeh93@gmail.com>
2025-11-20 00:52:45 +0330

    feat: Configure OpenAPI for automatic API documentation

---
+++ src/main/java/com/expensetracker/api/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.expensetracker.api.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI expenseTrackerOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Expense Tracker API")
+                        .description("Backend API for managing personal expenses")
+                        .version("1.0"))
+                .components(new Components()
+                        .addSecuritySchemes("basicAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("basic")));
+    }
+}
